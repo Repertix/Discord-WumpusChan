1b698c0f3b41ae5bf80b6c92edb5ab062d6e81a2
Reper <43357489+Repertix@users.noreply.github.com>
2021-05-31 15:51:44 -0500

    OOPS, where did this came from?

--- cmd/zawardo.js
+++ cmd/zawardo.js
@@ -1,11 +1,6 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, numero, prefix) => {
-  message.delete()
-  client.users.cache.get('344005754941997056').send("corazon partio")
-  client.users.cache.get('344005754941997056').send("corazon partio")
-  client.users.cache.get('344005754941997056').send("corazon partio")
-  client.users.cache.get('344005754941997056').send("corazon partio")
-  client.users.cache.get('344005754941997056').send("corazon partio")
+  //Nothing to see here.
 }
 
 module.exports.help = {

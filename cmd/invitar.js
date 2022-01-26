215fb9dbf86b723dbf13fbb84ad347a6d4088234
Reper <43357489+Repertix@users.noreply.github.com>
2022-01-26 16:39:28 -0600

    Update invitar.js

--- cmd/invitar.js
+++ cmd/invitar.js
@@ -3,7 +3,7 @@ const config = require('../config.json')
 module.exports.run = async (client, message, args, numero, prefix) => {
   const embed = new Discord.MessageEmbed()
   .setColor(config.color)
-  .addField("Invita a Wumpus Chan!", "[Invitar](https://discordapp.com/oauth2/authorize?client_id=724774897183883315&scope=bot&permissions=8)")
+  .addField("Invita a Wumpus Chan!", "[Invitar](BOT-INVITE-LINK)")
   message.channel.send(embed)
 }
 

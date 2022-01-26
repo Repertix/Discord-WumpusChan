547f820a336fe6e1a27ef1ca40594c103c8dfab1
Reper <43357489+Repertix@users.noreply.github.com>
2022-01-26 16:40:09 -0600

    Delete debugServer.js

--- cmd/debugServer.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-module.exports.run = async (client, message, args, numero, prefix) => {
-  message.channel.send("Miembros: "+client.users.cache.size)
-  message.channel.send(client.guilds.cache.map(o => o.name).join('\n'))
-}
-
-module.exports.help = {
-  name:"debugSvs"
-}
